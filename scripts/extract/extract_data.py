19dc6fb7a24ec6490085ffc3ca00f12eb46dbe48
mats-bb <mats.k.berg@gmail.com>
2024-04-30 12:19:40 +0200

    fixed up data extraction script

--- scripts/extract/extract_data.py
+++ scripts/extract/extract_data.py
@@ -1,7 +1,6 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
 import json
-import pandas as pd
 
 URL = "https://www.komplett.no/product/1303149/datautstyr/pc-komponenter/skjermkort/asus-dual-geforce-rtx-4070-super-oc-hvit"
 
@@ -13,28 +12,33 @@ headers = {
 
 def get_resp(url, headers):
     """Get response from url."""
+
     return req.get(url, headers=headers)
 
 
 def get_soup(resp):
     """Get soup object from response."""
+
     return bs(resp.content, "html.parser")
 
 
 def save_to_json(dir_, filename, data):
     """Save json file to directory."""
+
     with open(fr'{dir_}/{filename}.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
 
 def load_from_json(dir_, filename):
     """Load json file from directory."""
+
     with open(fr'{dir_}/{filename}.json', 'r', encoding='utf-8') as f:
         return json.load(f)
 
 
 def get_basic_data(soup):
     """Return basic product json."""
+
     basic_product_data = json.loads(soup.find("script", type="application/ld+json").string)
     
     return basic_product_data
@@ -51,6 +55,8 @@ def clean_basic_data(basic_product_data):
             clean_product_data[key] = val
 
     clean_product_data["producer"] = basic_product_data["brand"]["name"]
+
+    # Product out of stock. Adjust DB and script accordingly
     try:
         clean_product_data["price"] = basic_product_data["offers"]["price"]
     except KeyError:
@@ -58,16 +64,25 @@ def clean_basic_data(basic_product_data):
 
     return clean_product_data
 
-def get_product_categories(soup):
+def get_product_categories(url):
     """Return product categories."""
-    category_data = soup.find("script", id="breadcrumbsScript")
-    category_data_str = category_data.get("data-model")
-    category_data_dict = json.loads(category_data_str)
-    category_data_list = category_data_dict["breadCrumb"].split("|")[1:]
+
+    # category_data = soup.find("script", id="breadcrumbsScript")
+    # category_data_str = category_data.get("data-model")
+    # category_data_dict = json.loads(category_data_str)
+    # category_data_list = category_data_dict["breadCrumb"].split("|")[1:]
+    # category_out_dict = {
+    #     "main_category": category_data_list[0],
+    #     "type_category": category_data_list[1],
+    #     "product_category": category_data_list[2]
+    # }
+
+    categories_list = url.split("/")[5:8]
+
     category_out_dict = {
-        "main_category": category_data_list[0],
-        "type_category": category_data_list[1],
-        "product_category": category_data_list[2]
+        "main_category": categories_list[0],
+        "type_category": categories_list[1],
+        "product_category": categories_list[2]
     }
 
     return category_out_dict
@@ -83,10 +98,7 @@ def get_product_spec_sheet(soup):
     for table in spec_tables:
         table_dict = {}
 
-        try:
-            table_dict["caption"] = table.caption.text.lower()
-        except AttributeError:
-            print(json.loads(soup.find("script", type="application/ld+json").string)["name"])
+        table_dict["caption"] = table.caption.text.lower()
 
         for row in table.tbody.find_all('tr'):
             table_dict[row.th.text.lower()] = row.td.text.lower()
@@ -96,8 +108,10 @@ def get_product_spec_sheet(soup):
     return spec_list
 
 
-def combine_product_data(clean_product_data, soup):
-    product_categories = get_product_categories(soup)
+def combine_product_data(clean_product_data, soup, url):
+    """Combine the product data."""
+
+    product_categories = get_product_categories(url)
     product_spec_sheet = get_product_spec_sheet(soup)
 
     combined_product_data = clean_product_data 
@@ -108,6 +122,7 @@ def combine_product_data(clean_product_data, soup):
     return combined_product_data
 
 def get_all_products(urls):
+    """Combine all products. Return list of dicts."""
 
     all_products_info = []
     
@@ -116,7 +131,7 @@ def get_all_products(urls):
         soup = get_soup(resp)
         basic_product_data = get_basic_data(soup)
         clean_product_data = clean_basic_data(basic_product_data)
-        combined_product_data = combine_product_data(clean_product_data, soup)
+        combined_product_data = combine_product_data(clean_product_data, soup, url)
         all_products_info.append(combined_product_data)
 
     return all_products_info
