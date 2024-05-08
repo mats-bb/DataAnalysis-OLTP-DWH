7e89819bf9aebafe7e390e572ecb6c9066d7cee4
mats-bb <mats.k.berg@gmail.com>
2024-05-08 14:43:42 +0200

    changes to oltp ddl. added dwh ddl.

--- db/komplett.sql
+++ db/komplett.sql
@@ -31,33 +31,33 @@ card_info
 
 -- PRODUCT
 
-drop table if exists producer;
+drop table if exists producer cascade;
 create table producer (
 	id serial primary key,
 	producer_name varchar(50) unique not null
 	);
 
-drop table if exists main_category;
+drop table if exists main_category cascade;
 create table main_category (
 	id serial primary key,
 	category_name varchar(50) unique not null
 	);
 
-drop table if exists type_category;
+drop table if exists type_category cascade;
 create table type_category (
 	id serial primary key,
 	category_name varchar(50) unique not null,
 	main_cat_id int not null references main_category (id)
 	);
 
-drop table if exists product_category;
+drop table if exists product_category cascade;
 create table product_category (
 	id serial primary key,
 	category_name varchar(50) unique not null,
 	type_cat_id int not null references type_category (id)
 	);
 
-drop table if exists discount;
+drop table if exists discount cascade;
 create table discount (
 	id serial primary key,
 	discount_name varchar(50) unique not null,
@@ -68,7 +68,7 @@ create table discount (
 	end_date date not null
 	);
 	
-drop table if exists product;
+drop table if exists product cascade;
 create table product (
 	id serial primary key,
 	product_name varchar(75) unique not null,
@@ -77,14 +77,14 @@ create table product (
 	producer_id int not null references producer (id)
 	);
 
-drop table if exists product_discount;
+drop table if exists product_discount cascade;
 create table product_discount (
 	id serial primary key,
 	product_id int not null references product (id),
 	discount_id int not null references discount (id)
 	);
 
-drop table if exists price_history;
+drop table if exists price_history cascade;
 create table price_history (
 	id serial primary key,
 	price decimal(8, 2) not null,
@@ -92,28 +92,28 @@ create table price_history (
 	product_id int not null references product (id)
 	);
 
-drop table if exists product_image;
+drop table if exists product_image cascade;
 create table product_image (
 	id serial primary key,
 	img_path text unique not null,
 	product_id int not NULL references product (id)
 	);
 
-drop table if exists product_info;
+drop table if exists product_info cascade;
 create table product_info (
 	id serial primary key,
 	info_body jsonb not null,
 	product_id int not null references product (id)
 	);
 
-drop table if exists product_specs;
+drop table if exists product_specs cascade;
 create table product_specs (
 	id serial primary key,
 	spec_body jsonb not null,
 	product_id int not null references product (id)
 	);
 
-drop table if exists product_inventory;
+drop table if exists product_inventory cascade;
 create table product_inventory (
 	id serial primary key,
 	SKU varchar(50) unique not null,
@@ -125,20 +125,20 @@ create table product_inventory (
 
 -- ORDERS
 
-drop table if exists payment_provider;
+drop table if exists payment_provider cascade;
 create table payment_provider (
 	id serial primary key,
 	name varchar(30) unique not null
 	);
 
-drop table if exists delivery_option;
+drop table if exists delivery_option cascade;
 create table delivery_option (
 	id serial primary key,
 	type varchar(30) unique not null,
 	price decimal(3, 2) not null
 	);
 
-drop table if exists orders;
+drop table if exists orders cascade;
 create table orders (
 	id serial primary key,
 	order_date date not null,
@@ -151,17 +151,17 @@ create table orders (
 	customer_id int not null references customer (id)
 	);
 
-drop table if exists orders_payment;
+drop table if exists orders_payment cascade;
 create table orders_payment (
 	id serial primary key,
 	total_amount decimal(6, 2) not null,
 	payment_date date not null,
 	status varchar(20) default null,
 	orders_id int not null references orders (id),
-	card_id INt NOT NULL REFERENCES card_info (id)
+	card_id int NOT NULL REFERENCES card_info (id)
 	);
 
-drop table if exists orders_product;
+drop table if exists orders_product cascade;
 create table orders_product (
 	id serial primary key,
 	quantity int not null,
@@ -184,7 +184,7 @@ create table customer (
 	last_modified date DEFAULT current_date NOT null 
 	);
 
-drop table if exists state;
+drop table if exists state cascade;
 create table state (
 	id serial primary key,
 	state_name varchar(40) unique not null
@@ -205,14 +205,14 @@ create table address (
 	city_id int not null references city (id)
 	);
 
-drop table if exists customer_address;
+drop table if exists customer_address cascade;
 create table customer_address (
 	id serial primary key,
 	customer_id int not null references customer (id),
 	address_id int not null references address (id)
 	);
 
-drop table if exists card_info;
+drop table if exists card_info cascade;
 create table card_info (
 	id serial primary key,
 	card_number varchar(19) unique not null,
@@ -226,14 +226,14 @@ create table card_info (
 
 -- CART
 
-drop table if exists cart;
+drop table if exists cart cascade;
 create table cart (
 	id serial primary key,
 	created_date date not null,
 	customer_id int not null references customer (id)
 	);
 
-drop table if exists cart_product;
+drop table if exists cart_product cascade;
 create table cart_product (
 	id serial primary key,
 	quantity int not null,
@@ -243,7 +243,7 @@ create table cart_product (
 
 -- WISHLIST
 
-drop table if exists wishlist;
+drop table if exists wishlist cascade;
 create table wishlist (
 	id serial primary key,
 	wishlist_name varchar(70) not null,
@@ -252,7 +252,7 @@ create table wishlist (
 	customer_id int not null references customer (id)
 	);
 
-drop table if exists wishlist_product;
+drop table if exists wishlist_product cascade;
 create table wishlist_product (
 	id serial primary key,
 	quantity int not null,
@@ -262,7 +262,7 @@ create table wishlist_product (
 
 -- CUSTOMER_PRODUCT
 
-drop table if exists product_review;
+drop table if exists product_review cascade;
 create table product_review (
 	id serial primary key,
 	title text not null,
